b93c750b2aa0a15abb8ba2e08d10ea5cb0e2cc1a
Boyko Lalov <boiskila@gmail.com>
2021-01-24 00:19:52 +0200

    ignore dist

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var foo = 'hello from fooo';
-console.log(foo, express_1["default"]);
